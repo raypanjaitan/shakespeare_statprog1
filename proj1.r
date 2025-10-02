da289238136962535c118b09b321bcce45c6768e
Sanjoi Sethi <s2891732@sms.ed.ac.uk>
2025-10-02 23:46:21 +0100

    Completed 7 and part of 9

--- proj1.r
+++ proj1.r
@@ -9,25 +9,33 @@ a <- scan("shakespeare.txt", what="character", skip=83, nlines=196043-83,
 
 stage_dir=function(a) 
 {
-  rem_index=integer(0) #To store indices of the stage words to be removed
-  starting_bracket_index=grep("[", a, fixed=TRUE) #Indices of [
-  for(i in starting_bracket_index) #For stage direction words
+  if(length(grep("[", a, fixed=TRUE))==0)
   {
-    ending_bracket_counter=min(i+100, length(a)) #Counter to search for ]
-    stage_dir_words=a[i:ending_bracket_counter] #Staging direction words between []
-    ending_bracket_index=grep("]", stage_dir_words, fixed=TRUE)[1] #Indices of ] 
-    if(!is.na(ending_bracket_index)) #Checking that there exists a ]
-    {
-      j=i+ending_bracket_index-1 #Ending bracker index
-    } 
-    else 
-    {
-      j=i #No match for [
-    }
-    rem_index=c(rem_index, i:j) #All words from []
+    return(a) #Error-handling
   }
-  return(a[-rem_index]) #Removing the staging directions
+  else
+  {
+    rem_index=integer(0) #To store indices of the stage words to be removed
+    starting_bracket_index=grep("[", a, fixed=TRUE) #Indices of [
+    for(i in starting_bracket_index) #For stage direction words
+    {
+      ending_bracket_counter=min(i+100, length(a)) #Counter to search for ]
+      stage_dir_words=a[i:ending_bracket_counter] #Staging direction words between []
+      ending_bracket_index=grep("]", stage_dir_words, fixed=TRUE)[1] #Indices of ] 
+      if(!is.na(ending_bracket_index)) #Checking that there exists a ]
+      {
+        j=i+ending_bracket_index-1 #Ending bracker index
+      } 
+      else 
+      {
+        j=i #No match for [
+      }
+      rem_index=c(rem_index, i:j) #All words from []
+    }
+    return(a[-rem_index]) #Removing the staging directions
+  }
 }
+  
 
 a_1=stage_dir(a) #Calling the function to remove stage directions
 
@@ -84,7 +92,7 @@ eg_1_ans=split_punct(eg_1, punct_vec)
 eg_1_ans
 
 #4 (e) Using split_punct for our dataset
-a_4=split_punct(a_3[1:100000], punct_vec) #Running over partial dataset as the function is taking too long to run
+a_4=split_punct(a_3[1:10000], punct_vec) #Running over partial dataset as the function is taking too long to run
 
 #4(f) Converting the dataset to lowercase
 lowercase_words=function(a_4)
@@ -101,7 +109,6 @@ unique_words=function(a_5)
 }
 
 b_1=unique_words(a_5) #Passing the dataset to fetch the unique words
-b_1
 
 #5 (b) #Fetching the indices of unique words from a_5
 match_index=function(a_5, b_1)
@@ -110,7 +117,7 @@ match_index=function(a_5, b_1)
 }
 
 b_2=match_index(a_5,b_1) #Passing the datasets to get the indices of all occurences of the unique words in b present in a_5
-cat("Length of data vector is same as the index vector a_5:",(length(a_5)), "b:", length(b_1)) #Checking if length of data vector a_5 is same as the index vector b
+cat("Length of data vector is same as the index vector a_5:",(length(a_5)), "b:", length(b_2)) #Checking if length of data vector a_5 is same as the index vector b
 
 #5 (c) Counting the occurences of unique words
 tabulation=function(b_2, b_1)
@@ -121,19 +128,16 @@ tabulation=function(b_2, b_1)
 }
 
 b_3=tabulation(b_2, b_1) #Passing the index vector to tabulate it
-b_3
 
 #5 (d) Finding the most common 1000 words
 common=function(b_3)
 {
   ranks=rank(-b_3, ties.method="first") #Giving ascending rank to words with max occurences
   common_words=names(ranks)[ranks<=1000] #Choosing top 1000 words
-  common_words_asc=common_words[order(ranks[common_words])] #Sorting the words in ascending order according to their ranks
-  return(common_words_asc)
+  return(common_words)
 }
 
 b=common(b_3)
-b
 
 #6 (a) Creating the token vector
 token_generation=function(a_5,b)
@@ -143,7 +147,6 @@ token_generation=function(a_5,b)
 }
 
 tokens=token_generation(a_5,b)
-tokens
 cat("Length of data vector is same as the tokens vector a_5:",(length(a_5)), "tokens:", length(tokens)) #Checking if length of data vector a_5 is same as the tokens vector
 
 #6 (b) Creating matrix M
@@ -154,28 +157,74 @@ matrix_creation=function(n, mlag, tokens)
 }
 
 M=matrix_creation(length(a_5), mlag=4, tokens)
-M
 
+#7 Generation model
+next.word=function(key,M,M1,w=rep(1,ncol(M)-1)) #Pass M1 from outside
+{
+  #Pre-processing of key
+key_1=stage_dir(key)
+  key_2=upper_numeral(key_1)
+  key_3=hyphen_underscore(key_2)
+  key_4=split_punct(key_3, punct_vec) #Using punct_vec as global
+  key_5=lowercase_words(key_4)
+  
+  #Choosing last mlag words from key
+  mlag=ncol(M)-1
+  length_check=min(mlag, length(key_5))
+  key_6=tail(key_5, length_check)
+  
+  #Making tokens of key
+  token_key=token_generation(key_6,b)
+  prob=setNames(rep(0, length(b)),1:length(b))
+  
+  for(mc in 1:mlag)
+  {
+    reduced_token=head(token_key[mc:mlag]) #Decreasing the token length as the size of matrix M decreases
+    ii=colSums(!(t(M[,mc:mlag,drop=FALSE])==reduced_token)) 
+    matching=grep(0, ii, fixed=TRUE) #Counting the number of zeros for all matches
+    if(length(matching)>0) 
+    {
+      u=M[matching, mlag+1] #Finding the corresponding last column entry of M wherever we found a match
+      prob_mc=(1/mlag)*table(u)/length(u) #Finding the probability distribution over b
+      prob[names(prob_mc)]=prob[names(prob_mc)]+prob_mc
+    }
+    # Commented because getting an unresolved error
+    # else
+    # {
+    #   #Sampling a common word, and assigning it highest probability for it to be selected
+    #   tab=table(a_5)
+    #   frequencies=tab[as.character(b)]
+    #   frequencies[is.na(frequencies)] <- 0
+    #   probabilities=frequencies/length(a_5)
+    #   most_probable_word=sample(x=b,size=1,prob=probabilities)
+    #   most_probable_token=grep(most_probable_word,b,fixed=TRUE)
+    #   prob_mc=(1/mlag)*c(most_probable_token=1)
+    #   prob[names(prob_mc)]=prob[names(prob_mc)]+prob_mc
+    # }
+  }
+  most_probable_tokens=names(prob)[which(prob==max(prob))] #Tokens with highest probability (maybe multiple)
+  most_probable_token=as.integer(sample(most_probable_tokens,1)) #Sampling if multiple tokens in the previous step (Introduces randomness)
+  most_probable_word=b[most_probable_token] #Converting token to word
+  return(most_probable_word) 
+}
 
+predicted_word=next.word(key=a_5[1:10], M, tokens)
 
+#9 Sentence generation till full-stop
+sentence_generation=function(initial){
+  sentence=c(initial)
+  while (!(tail(sentence,1) %in% c(".",","))) #Waiting for a full-stop takes a long time
+  {
+    next_word=next.word(key=sentence, M, tokens)
+    sentence=c(sentence,next_word)
+  }
+  return(sentence)
+}
 
+raw_sentence=sentence_generation(predicted_word)
 
+processed_output=function(raw_sentence){
+  return(paste(raw_sentence, collapse = " "))
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+processed_output(raw_sentence)
