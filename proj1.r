24a3167d19e26f4f25eeade44f2642d8ad700845
Sanjoi Sethi <s2891732@sms.ed.ac.uk>
2025-10-02 07:04:21 +0100

    Removed the Uppercase Letters and Arabic Numerals

--- proj1.r
+++ proj1.r
@@ -9,28 +9,51 @@ a <- scan("shakespeare.txt",what="character",skip=83,nlines=196043-83,
           fileEncoding="UTF-8") #Reading the dataset in UTF-8 text encoding, in character type form. We are skipping the first 83 lines of the dataset.
 
 #4 (a) Removing the stage directions
-stage_dir=function(starting_bracket_index, a)
-{
-  counter=0 #For noting the index of ]
-  for (i in 0:100)
-  {
-    #Finding the index of ] and storing it in the counter variable and breaking from the loop as soon as a ] is found
-    if(grepl(']', a[starting_bracket_index+i]))
-    {
-      counter=i
-      break
-    }
+# stage_dir=function(starting_bracket_index, a)
+# {
+#   stage_dir_words=c()
+#   for (i in starting_bracket_index)
+#   {
+#     stage_dir_words=c()
+#     counter=0 #For noting the index of ]
+#     for (j in 0:100)
+#     {
+#       #Finding the index of ] and storing it in the counter variable and breaking from the loop as soon as a ] is found
+#       if(grepl("]", a[i+j]))
+#       {
+#         counter=j
+#         break
+#       }
+#     }
+#     stage_dir_words=c(stage_dir_words, a[i:(i+counter)])#Extracting the words between []
+#   }
+#   a_1=a[is.na(match(a,stage_dir_words))] #Deleting the stage words 
+#   return(a_1)
+# }
+# 
+# starting_bracket_index=grep("[", a, fixed=TRUE) #Fetching the indices of the opening brackets
+# a_1=stage_dir(starting_bracket_index, a)
+
+open_br<-grep("[", a, fixed=TRUE)
+del1<-c()
+for (i in open_br){
+  close_br=grep("]",a[i:i+100],fixed=TRUE)
+  if (length(close_br)>0){
+    close_index<-i + close_br[1]-1
+    del1 <- c(del1, i:close_index)
   }
-  stage_dir_words=a[starting_bracket_index:(starting_bracket_index+i)] #Extracting the words between []
-  a_1=a[is.na(match(a,stage_dir_words))] #Deleting the stage words 
-  return(a_1)
+}
+a_1=a[-del1] 
+
+#4 (b) Removing character names (Fully Uppercase Words) and Arabic Numerals
+upper_numeral=function(a_1)
+{
+  a_upper=a==toupper(a) & !(a %in% c('I','A')) #Fetching the uppercase words except I and A
+  a_upper_rem=a_upper[!a_upper] #Removing the uppercase words
+  a_numeral=gsub("[0-9]", "", a_upper_rem) #Removing Arabic numerals
 }
 
-starting_bracket_index=grep("[", a, fixed=TRUE) #Fetching the indices of the opening brackets
-a_1=stage_dir(a) #Passing it to the function
-
-
-
+a_2=upper_numeral(a_1) #Passing the dataset without the stage words
 
 
 
